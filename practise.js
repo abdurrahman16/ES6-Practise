bffda6373f9f01ef32e71500518feb580fef16e1
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-31 23:18:19 +0600

    3dot operation

--- practise.js
+++ practise.js
@@ -20,12 +20,26 @@
 // const result = doubleIt(5);
 // console.log(result);
 
-const longFunction = (x,y) => {
-    const  sum = x+y;
-    const diff = x-y;
-    const result = sum*diff;
-    return result;
-}
+// const longFunction = (x,y) => {
+//     const  sum = x+y;
+//     const diff = x-y;
+//     const result = sum*diff;
+//     return result;
+// }
 
-const result = longFunction(6,7);
+// const result = longFunction(6,7);
+// console.log(result);
+
+
+// const ages =[11,14,16,12,18];
+// const ages2 =[22,12,14,17,19];
+// const ages3 =[22,45,19,71,29];
+
+
+// const allAges =ages.concat(ages2).concat(ages3);
+// const allAges3 = [...ages,...ages2,...ages3,1000];
+// console.log(allAges3);
+
+const TakaPoisha = ['12','190','99900'];
+const result = Math.max(...TakaPoisha);
 console.log(result);
